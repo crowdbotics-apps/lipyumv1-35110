a2d020490406d991837e36ad3b112508a71ac3d5
Crowdbotics <team@crowdbotics.com>
2022-05-06 06:55:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lipyumv1_35110/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lipyumv1_35110;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "lipyumv1_35110";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lipyumv1_35110",
+  url: "https://lipyumv1_35110.botics.co",
+  api: "https://lipyumv1_35110.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
